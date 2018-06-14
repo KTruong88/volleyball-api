d3c225b9397e8c277134ebe10e9b2cea087d015d
Kevin Truong <kevintru0ng@yahoo.com>
2018-06-14 13:27:04 -0700

    match_id name?

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,7 +4,7 @@ class MatchesController < ApplicationController
   # GET /matches
   # GET /matches.json
   def index
-    @matches = Match.all
+    @matches = Match.joins(:teams)
     render json: {matches: @matches}
   end
 
