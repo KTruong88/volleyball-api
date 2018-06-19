8e3559eb1b95fb8f4e541798da7d03f048c1ecea
Kevin Truong <kevintru0ng@yahoo.com>
2018-06-19 15:19:37 -0700

    change home route to matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,7 +5,7 @@ class MatchesController < ApplicationController
   # GET /matches.json
   def index
     matches_sql = '
-    SELECT matches.id, home_team.name AS home_team, away_team.name AS away_team
+    SELECT m.id, home_team.name AS home_team, away_team.name AS away_team
     FROM Matches as m
     INNER JOIN teams AS home_team ON home_team.id=m.home_team_id
     INNER JOIN teams AS away_team ON away_team.id=m.away_team_id'
