99fa841cdcc6c8e3ba179d607d2a7d4f9a2e0942
Kevin Truong <kevintru0ng@yahoo.com>
2018-06-19 15:09:06 -0700

    change home route to matches

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :matches
   resources :users
 
-  root 'users#index'
+  root 'matches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
